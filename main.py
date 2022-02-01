4a9c04d7581c035c2722883d7174b9472d9de889
Theo <theosmithscott@protonmail.com>
2022-02-01 20:16:23 +0000

    Update main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from modules.crypto import Crypto
+import base64
+
+
+handler = Crypto()
+
+chat = open("crypt/chat.enc", "wb")
+
+with chat:
+    chat.write(base64.b64encode(handler.encrypt("Hello World!")))
