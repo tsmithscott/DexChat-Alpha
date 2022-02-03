92e1b244783a0f58982cfbe0dff96f61f8ee63a9
Theo <theosmithscott@protonmail.com>
2022-02-03 15:08:07 +0000

    Created encyption module
    - Moved crypto.py and keychain.py to this module

--- modules/encryption/crypto.py
+++ modules/encryption/crypto.py
@@ -4,7 +4,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import serialization, hashes
 
-from modules.keychain import Keychain
+from modules.encryption.keychain import Keychain
 
 
 class Crypto:
