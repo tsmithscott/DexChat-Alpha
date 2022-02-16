94d355b5e6b41cbd0b2f3472e77366e3ee4f12d4
Theo <theosmithscott@protonmail.com>
2022-02-16 23:34:42 +0000

    Update communication.py

--- modules/communication.py
+++ modules/communication.py
@@ -1,12 +1,13 @@
 import socket
 import sys
+import threading
 
 
 class SingleConnection:
     def __init__(self, host, port):
+        self.peers = []
         self.host = host
         self.port = port
-
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server.bind(("0.0.0.0", 25000))
@@ -18,14 +19,30 @@ class SingleConnection:
         while True:
             connection, address = self.server.accept()
 
-            self.server_receive(connection)
+            print("working-1")
 
-    def server_receive(self, connection):
+            new_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+            print("2")
+            new_client.connect((address[0], 25000))
+
+            print("3")
+            self.peers.append(new_client)
+
+            print("working")
+            print(self.peers)
+
+            initiate = threading.Thread(target=self.server_receive, args=(connection,), daemon=True)
+            initiate.start()
+
+    @staticmethod
+    def server_receive(connection):
         while True:
             try:
                 message = connection.recv(4096)
 
                 if message.decode() == "/disconnect":
+                    connection.close()
                     sys.exit()
                 else:
                     print(message.decode())
@@ -40,6 +57,7 @@ class SingleConnection:
             if message == "/connect":
                 try:
                     self.client.connect((self.host, self.port))
+                    self.peers.append(self.client)
                 except ConnectionError as error:
                     print(error)
             elif message == "/disconnect":
@@ -50,4 +68,5 @@ class SingleConnection:
                 except ConnectionError as error:
                     print(error)
             else:
-                self.client.send(message.encode())
+                for peer in self.peers:
+                    peer.send(message.encode())
