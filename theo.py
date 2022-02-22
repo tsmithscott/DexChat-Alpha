a9a53e79c769c73bd8b9d95b20e611c0ce0ff61a
Theo <theosmithscott@protonmail.com>
2022-02-22 16:22:37 +0000

    Update theo.py

--- theo.py
+++ theo.py
@@ -1,8 +1,8 @@
-from modules.communication import SingleConnection
+from modules.communication import Network
 import threading
 
 
-app = SingleConnection("82.0.10.30", 25000)
+app = Network("82.0.10.30", 25000)
 
 threading.Thread(target=app.server_accept).start()
 threading.Thread(target=app.client_send).start()
