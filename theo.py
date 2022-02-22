274690e0c0f6f296bb8c0ed29870d678a85746d7
Theo <theosmithscott@protonmail.com>
2022-02-22 16:54:35 +0000

    Update theo.py

--- theo.py
+++ theo.py
@@ -4,5 +4,5 @@ import threading
 
 app = Network("82.0.10.30", 25000)
 
-threading.Thread(target=app.server_accept).start()
+threading.Thread(target=app.server_accept, daemon=True).start()
 threading.Thread(target=app.client_send).start()
