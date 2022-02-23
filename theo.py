33a573af586180dc5cb7d04b70597ec3fb8867a7
Theo <theosmithscott@protonmail.com>
2022-02-23 21:25:12 +0000

    Update theo.py

--- theo.py
+++ theo.py
@@ -4,5 +4,5 @@ import threading
 
 app = VoiceNetwork("82.0.10.30", 25000)
 
-threading.Thread(target=app.server_receive(), daemon=True).start()
+threading.Thread(target=app.server_receive, daemon=True).start()
 threading.Thread(target=app.client_send, daemon=True).start()
